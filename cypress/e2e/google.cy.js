c324eeb495c17850d9878a3f645deffb90527104
Terk <Redrahn.001@gmail.com>
2026-06-17 14:52:07 +0700

    init cypress ci learning project

--- cypress/e2e/google.cy.js
+++ cypress/e2e/google.cy.js
@@ -1,6 +1,6 @@
 describe("Google", () => {
   it("visit google", () => {
     cy.visit("https://www.google.com");
-    cy.title().should("contain", "FaceBook");
+    cy.title().should("contain", "Google");
   });
 });
