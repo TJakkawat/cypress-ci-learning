188e455e4b9256956ed9f54577eb4caad2aabcd7
Terk <Redrahn.001@gmail.com>
2026-06-19 10:00:16 +0700

    add google homepage tests

--- cypress/e2e/google.cy.js
+++ cypress/e2e/google.cy.js
@@ -1,6 +1,22 @@
-describe("Google", () => {
-  it("visit google", () => {
+describe("Google Search", () => {
+  beforeEach(() => {
     cy.visit("https://www.google.com");
-    cy.title().should("contain", "Google");
+  });
+
+  it("should display Google homepage correctly", () => {
+    cy.title().should("eq", "Google");
+
+    cy.get("textarea[name='q']")
+      .should("be.visible")
+      .and("have.attr", "name", "q");
+  });
+
+  it("should allow user to type in search box", () => {
+    cy.get("textarea[name='q']")
+      .should("be.visible")
+      .type("Cypress Testing");
+
+    cy.get("textarea[name='q']")
+      .should("have.value", "Cypress Testing");
   });
 });
